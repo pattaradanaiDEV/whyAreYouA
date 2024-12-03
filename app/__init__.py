df19e02cb27bf4b4a0e80a939169afab824b72a8
bruh045 <pattaradanai_chaitan@cmu.ac.th>
2024-12-03 15:45:46 +0700

    lab03

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
+
+
 app = Flask(__name__, static_folder='static')
-from app import views # noqa
+
+
+app.jinja_options = app.jinja_options.copy()
+app.jinja_options.update({
+    'trim_blocks': True,
+    'lstrip_blocks': True
+})
+
+
+app.config['DEBUG'] = True
+
+
+from app import views  # noqa
--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-gunicorn main:app --chdir app -w 2 --threads 2 -b 0.0.0.0:8000 --reload
+if [ "$FLASK_DEBUG" = "1" ]; then
+    echo "Running on Flask Development Server"
+    python3 main.py
+else
+    echo "Running on gunicorn"
+    gunicorn main:app -c "$PWD"/gunicorn.config.py
+fi
 exec "$@"
