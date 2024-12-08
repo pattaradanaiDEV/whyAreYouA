8ca3da855b4c11f4b9cc24b355833d3750f45797
bruh045 <pattaradanai_chaitan@cmu.ac.th>
2024-12-08 20:20:20 +0700

    gomen amanai

--- app/hw_views.py
+++ app/hw_views.py
@@ -26,17 +26,13 @@ def api_weather():
 
 @app.route('/hw03/pm25/')
 def hw03_pm25():
-    api="https://api.waqi.info/feed/beijing/?token=db32416270f0dbae5bbfbda1974152ab2d1bd961"
+    api="https://api.waqi.info/feed/chiangmai/?token=db32416270f0dbae5bbfbda1974152ab2d1bd961"
     apigot=json.load(urlopen(api))
-    #apigot = json.loads("testJSON.json")
     data=apigot['data']['forecast']['daily']['pm25']
     first_day=int(data[0]['day'].split("-")[-1])
     if( first_day>7 ):
         first_day=first_day%7
     day_left=len(data)-(7-first_day)
-
-    num_of_week=day_left//7
-
-    fd_lw=(7*num_of_week)+(7-first_day)
-    din_last_week=day_left%7
-    return render_template('lab03/hw03_pm25.html',pmData=data,fday=first_day,num_ow=num_of_week,din_lw=din_last_week,fd_inlw=fd_lw,dl=day_left)
+    num_of_week=(day_left-1)//7
+    id_fdlw=(7*(day_left//7))+(7-first_day)+1
+    return render_template('lab03/hw03_pm25.html',pmData=data,fday=first_day,num_ow=num_of_week,fd_inlw=id_fdlw,dl=day_left)
