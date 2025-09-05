8c2e1c8fc16049734433aae84a9709539a87d2dd
KittYa18 <kittisakwichaiya2004@gmail.com>
2025-09-05 19:21:54 +0700

    db page text and dark bkg and text color

--- flask/app/views.py
+++ flask/app/views.py
@@ -2,27 +2,43 @@ import json
 from flask import (jsonify, render_template,
                   request, url_for, flash, redirect)
 
-
 from sqlalchemy.sql import text
 from app import app
 from app import db
 
-
 @app.route('/')
 def home():
-    return "Flask says 'Hello world!'"
-
-
-
+    return '\
+        <body style="background-color: black">\
+            <div style="text-align: center">\
+                <h1 style="color: gray">\
+                    Flask says "Hello world!\
+                </h1>\
+            </div>\
+        </body>'
 
 @app.route('/db')
 def db_connection():
     try:
         with db.engine.connect() as conn:
             conn.execute(text("SELECT 1"))
-        return '<h1>db works.</h1>'
+        return '\
+            <body style="background-color: black">\
+                <div style="text-align: center">\
+                    <h1 style="color:gray">\
+                        db works.\
+                    </h1>\
+                </div>\
+            </body>'
     except Exception as e:
-        return '<h1>db is broken.</h1>' + str(e)
+        return '\
+            <body style="background-color: black">\
+                <div style="text-align: center">\
+                    <h1 style="color:gray">\
+                        db is broken. return: 'f"{e}"'\
+                    </h1>\
+                </div>\
+            </body>'
 
 
 
