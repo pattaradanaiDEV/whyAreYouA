b3004d45d04a504458d54c76474f4e4e42a34237
bruh045 <pattaradanai_chaitan@cmu.ac.th>
2025-09-10 19:40:22 +0700

    add file category.py and item.py in folder models

---
+++ flask/app/models/category.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Category(db.Model):
+    __tablename__ = "Category"
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    cateName = db.Column(db.String(50))
+    items = db.relationship("items", back_populates="category")
+    def __init__(self,cateName):
+        self.cateName = cateName
+    
---
+++ flask/app/models/item.py
@@ -0,0 +1,35 @@
+import qrcode
+import base64
+from io import BytesIO
+from app import db
+
+class Item(db.Model):
+    __tablename__ = "Item"
+
+    itemID = db.Column(db.Integer, primary_key=True)
+    cateID = db.Column(db.Integer, db.ForeignKey("stocks_category.cateID"), nullable=False)
+    itemAmount = db.Column(db.Integer)
+    itemName = db.Column(db.String(100))
+    itemPicture = db.Column(db.String(255))
+    QR_Barcode = db.Column(db.Text)
+    category = db.relationship("Category", back_populates="items")
+    
+    def __init__(self,name,amount,picture):
+        self.ItemName = name
+        self.ItemAmount = amount
+        self.ItemPicture = picture
+        self.QR_Barcode = self.generate_qr(name)
+    
+    def generate_qr(self, data):
+        qr = qrcode.make(data)
+        buffer = BytesIO()
+        qr.save(buffer, format="PNG")
+        qr_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        return qr_b64
+        
+        
+    def update(self,name,amount,picture):
+        self.itemName=name
+        self.itemAmount=amount
+        self.itemPicture=picture
+        
