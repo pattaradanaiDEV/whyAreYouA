8b830179934cf885bbb0d1a7d1f775762e8892f1
prsns05 <purich.senasang@gmail.com>
2025-09-23 12:11:22 +0700

    fix theme

--- flask/app/templates/darkmode.js
+++ flask/app/templates/darkmode.js
@@ -1,20 +1,22 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode','active')
-}
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+};
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+if (themeSwitch) {
+    themeSwitch.addEventListener("click", () => {
+        darkmode = localStorage.getItem('darkmode');
+        darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+    });
 }
-
-if(darkmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
-   
