12ad854bf091052a47a7a6ce5b2eca2b30aee7a7
KittYa18 <kittisakwichaiya2004@gmail.com>
2025-10-07 17:23:40 +0700

    small fixes

--- flask/manage.py
+++ flask/manage.py
@@ -69,49 +69,49 @@ def seed_db():
     db.session.commit()
     
     item = Item(ItemName="Ballpoint Pen", ItemAmount=120, ItemPicture="Ballpoint_Pen.jpg", itemMin=60)
-    item.cateID = 1
+    item.cateID = 2
     db.session.add(item)
     item = Item(ItemName="Notebook A5", ItemAmount=75, ItemPicture="Notebook_A5.jpg", itemMin=30)
-    item.cateID = 1
+    item.cateID = 2
     db.session.add(item)
     item = Item(ItemName="Highlighter Set", ItemAmount=30, ItemPicture="Highlighter_Set.jpg", itemMin=15)
-    item.cateID = 1
+    item.cateID = 2
     db.session.add(item)
     item = Item(ItemName="Stapler", ItemAmount=20, ItemPicture="Stapler.jpg", itemMin=10)
-    item.cateID = 1
+    item.cateID = 2
     db.session.add(item)
     item = Item(ItemName="Paper Clips Box", ItemAmount=50, ItemPicture="Paper_Clips_Box.jpg", itemMin=25)
-    item.cateID = 1
+    item.cateID = 2
     db.session.add(item)
     item = Item(ItemName="Laptop Dell XPS", ItemAmount=10, ItemPicture="Laptop_Dell_XPS.jpg", itemMin=5)
-    item.cateID = 3
+    item.cateID = 4
     db.session.add(item)
     item = Item(ItemName="Wireless Mouse", ItemAmount=40, ItemPicture="Wireless_Mouse.jpg", itemMin=20)
-    item.cateID = 3
+    item.cateID = 4
     db.session.add(item)
     item = Item(ItemName="Mechanical Keyboard", ItemAmount=25, ItemPicture="Mechanical_Keyboard.jpg", itemMin=12)
-    item.cateID = 3
+    item.cateID = 4
     db.session.add(item)
     item = Item(ItemName="External HDD 1TB", ItemAmount=15, ItemPicture="External_HDD_1TB.jpg", itemMin=7)
-    item.cateID = 3
+    item.cateID = 4
     db.session.add(item)
     item = Item(ItemName="Monitor 24", ItemAmount=10, ItemPicture="Monitor_24.jpg", itemMin=5)
-    item.cateID = 3
+    item.cateID = 4
     db.session.add(item)
     item = Item(ItemName="Hammer", ItemAmount=18, ItemPicture="Hammer.jpg", itemMin=20)
-    item.cateID = 5
+    item.cateID = 6
     db.session.add(item)
     item = Item(ItemName="Screwdriver Set", ItemAmount=25, ItemPicture="Screwdriver_Set.jpg", itemMin=10)
-    item.cateID = 5
+    item.cateID = 6
     db.session.add(item)
     item = Item(ItemName="Wrench", ItemAmount=12, ItemPicture="Wrench.jpg", itemMin=5)
-    item.cateID = 5
+    item.cateID = 6
     db.session.add(item)
     item = Item(ItemName="Electric Drill", ItemAmount=8, ItemPicture="Electric_Drill.jpg", itemMin=8)
-    item.cateID = 5
+    item.cateID = 6
     db.session.add(item)
     item = Item(ItemName="Tape Measure", ItemAmount=30, ItemPicture="Tape_Measure.jpg", itemMin=15)
-    item.cateID = 5
+    item.cateID = 6
     db.session.add(item)
     db.session.commit()
 
