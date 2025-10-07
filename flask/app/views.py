7f6929d5b7e5e32fbb2ce3faf7c05764261aec7f
Ninkochan <nitipat_n@cmu.ac.th>
2025-10-07 19:32:08 +0700

    reload waiting page while you availiable is true redirect to home page

--- flask/app/views.py
+++ flask/app/views.py
@@ -96,6 +96,8 @@ def landing():
 
 @app.route('/waiting')
 def waiting():
+    if current_user.availiable == True:
+        return redirect(url_for('homepage'))
     return f"รอก่อนไอ่น้อง"
 
 @app.route('/homepage')
